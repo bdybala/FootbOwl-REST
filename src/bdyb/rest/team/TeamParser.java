5367c70ccbbfb972c4a2d9aaa91a9c4964e3c6b6
Bartosz Dybała <dybala.bartosz@gmail.com>
2017-01-16 01:42:08 +0100

    fix GET team, add stats_id and photo

--- src/bdyb/rest/team/TeamParser.java
+++ src/bdyb/rest/team/TeamParser.java
@@ -9,13 +9,14 @@ public class TeamParser {
 
 	private static final String DB_ID = 		"team_id";
 	private static final String DB_MANAGER_ID =	"man_acc_id";
+	private static final String DB_STATS_ID =	"stats_id";
 	private static final String DB_LEAGUE_ID =	"league_id";
 	private static final String DB_FULL_NAME = 	"full_name";
 	private static final String DB_ABB_NAME =	"abb_name";
 	private static final String DB_COUNTRY = 	"country";
 	private static final String DB_COUNTY = 	"county";
 	private static final String DB_TOWN = 		"town";
-//	private static final String DB_PHOTO =		"photo";
+	private static final String DB_PHOTO =		"photo";
 	
 	
 	static List<Team> parseListFromResultSet(ResultSet rs) {
@@ -27,12 +28,14 @@ public class TeamParser {
 
 				t.setiD(			rs.getInt(		DB_ID));
 				t.setManageriD(		rs.getInt(		DB_MANAGER_ID));
+				t.setStatsiD(		rs.getInt(		DB_STATS_ID));
 				t.setLeagueiD(		rs.getInt(		DB_LEAGUE_ID));
 				t.setFullName(		rs.getString(	DB_FULL_NAME));
 				t.setAbbreviation(	rs.getString(	DB_ABB_NAME));
 				t.setCountry(		rs.getString(	DB_COUNTRY));
 				t.setCounty(		rs.getString(	DB_COUNTY));
 				t.setTown(			rs.getString(	DB_TOWN));
+				t.setPhoto(			rs.getBytes(	DB_PHOTO));
 
 				teamList.add(t);
 			}
